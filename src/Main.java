fe0b39645f16d51bf21d1c8d992a577cce2fca7a
Fujun Wu <fwu@altheadx.com>
2014-03-06 14:10:14 -0800

    A V1.3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.printf("A master V1.1");
-		// Code added by forked repo
+		System.out.printf("A master V1.2");
+		// Code copied from the base
 	}
 
 }
