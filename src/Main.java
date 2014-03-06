82cea3e9ad0fbbb6bf411c181c3d1e38148e0bd0
Fujun Wu <fwu@altheadx.com>
2014-03-06 15:49:07 -0800

    FW V3.1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.printf("FW V3.1");
 		// Code added in the base
-		System.out.printf("A master V3.0");
-		// Code copied from the base
 
 	}
 
